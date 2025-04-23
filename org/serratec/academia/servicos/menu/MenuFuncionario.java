c9783146526ed5252b0c7d540740c428156a22c1
Domingos Machado <d.machado1993@gmail.com>
2025-04-23 18:55:10 -0300

    Update MenuFuncionario.java
    melhorando métodos cadastrarAluno e cadastrarPersonal

--- org/serratec/academia/servicos/menu/MenuFuncionario.java
+++ org/serratec/academia/servicos/menu/MenuFuncionario.java
@@ -14,169 +14,203 @@ import org.serratec.academia.modelo.Personal;
 import org.serratec.academia.modelo.Pessoa;
 
 public class MenuFuncionario implements Menu {
-    Scanner sc = new Scanner(System.in);
-    private static List<Pessoa> pessoas = new ArrayList<>();
+	Scanner sc = new Scanner(System.in);
+	private static List<Pessoa> pessoas = new ArrayList<>();
 
-    @Override
-    public void exibirMenu(Pessoa pessoa) {
-        pessoas.add(pessoa);
-        Funcionario funcionario = (Funcionario) pessoa;
-        
-        int opcao;
-        do {
-            System.out.println("\n# ===== # Menu de Funcionarios # ===== #");
-            System.out.println("1. Emitir Relatorios\n2. Ver Valor total a receber no Mês");
-            if(funcionario.getCargo() == Cargo.GERENTE) {
-                System.out.println("3. Cadastrar novo Aluno"
-                        + "\n4. Cadastrar novo Plano"
-                        + "\n5. Cadastrar novo Personal");
-            }
-            System.out.println("0. Sair");
-            System.out.print("Escolha uma opção: ");
-            opcao = sc.nextInt();
-            switch (opcao) {
-            case 1: 
-                emitirRelatorio();
-                break;
-            case 2: 
-                gerarValorTotal();
-                break;
-            case 3: 
-                if(funcionario.getCargo() == Cargo.GERENTE) {
-                    cadastrarAluno();
-                } else {
-                    System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
-                }
-                break;
-            case 4: 
-                if(funcionario.getCargo() == Cargo.GERENTE) {
-                    cadastrarPlano();
-                } else {
-                    System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
-                }
-                break;
-            case 5: 
-                if(funcionario.getCargo() == Cargo.GERENTE) {
-                    cadastrarPersonal();
-                } else {
-                    System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
-                }
-                break;
-            case 0: 
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida! Tente novamente.");
-            }
-        } while (opcao != 0);
-    }
+	@Override
+	public void exibirMenu(Pessoa pessoa) {
+		pessoas.add(pessoa);
+		Funcionario funcionario = (Funcionario) pessoa;
 
-    private void gerarValorTotal() {
-        double valorTotal = 0;
-        DecimalFormat deci = new DecimalFormat("0.00");
-        
-        for (Pessoa p : pessoas) {
-            if (p instanceof Aluno) {
-                Aluno aluno = (Aluno) p;
-                valorTotal += aluno.getPlano().getValor();
-            }
-        }
-        System.out.println("O Total para se receber nesse mês é: R$" + deci.format(valorTotal));
-    }
-    
-    private void cadastrarPersonal() {
-        System.out.print("Informe o nome do Personal: ");        
-        sc.nextLine();
-        String nome = sc.nextLine();
+		int opcao;
+		do {
+			System.out.println("\n# ===== # Menu de Funcionarios # ===== #");
+			System.out.println("1. Emitir Relatorios\n2. Ver Valor total a receber no Mês");
+			if (funcionario.getCargo() == Cargo.GERENTE) {
+				System.out.println(
+						"3. Cadastrar novo Aluno" + "\n4. Cadastrar novo Plano" + "\n5. Cadastrar novo Personal");
+			}
+			System.out.println("0. Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = sc.nextInt();
+			switch (opcao) {
+			case 1:
+				emitirRelatorio();
+				break;
+			case 2:
+				gerarValorTotal();
+				break;
+			case 3:
+				if (funcionario.getCargo() == Cargo.GERENTE) {
+					cadastrarAluno();
+				} else {
+					System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
+				}
+				break;
+			case 4:
+				if (funcionario.getCargo() == Cargo.GERENTE) {
+					cadastrarPlano();
+				} else {
+					System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
+				}
+				break;
+			case 5:
+				if (funcionario.getCargo() == Cargo.GERENTE) {
+					cadastrarPersonal();
+				} else {
+					System.out.println("Acesso negado. Apenas gerentes podem acessar esta função.");
+				}
+				break;
+			case 0:
+				System.out.println("Saindo...");
+				break;
+			default:
+				System.out.println("Opção inválida! Tente novamente.");
+			}
+		} while (opcao != 0);
+	}
 
-        String cpf;
-        boolean cpfValido;
+	private void gerarValorTotal() {
+		double valorTotal = 0;
+		DecimalFormat deci = new DecimalFormat("0.00");
 
-        do {
-            System.out.print("Informe o CPF (Apenas Números): ");
-            cpf = sc.nextLine();
-            cpf = cpf.replaceAll("[^0-9]", "");
-            cpfValido = validarCPF(cpf);
+		for (Pessoa p : pessoas) {
+			if (p instanceof Aluno) {
+				Aluno aluno = (Aluno) p;
+				valorTotal += aluno.getPlano().getValor();
+			}
+		}
+		System.out.println("O Total para se receber nesse mês é: R$" + deci.format(valorTotal));
+	}
 
-            if (!cpfValido) {
-                System.out.println("Por favor, informe um CPF válido.");
-            }
-        } while (!cpfValido);
-        
-        System.out.print("Informe a senha do Personal: ");
-        String senha = sc.nextLine();
-        System.out.print("Informe a especialidade do Personal: ");
-        String espec = sc.nextLine();
-        System.out.print("Informe a CREF do Personal: ");
-        String cref = sc.nextLine();
-        pessoas.add(new Personal(nome, cpf, senha, espec, cref));
-        System.out.println("Personal " + nome + " cadastrado(a) com sucesso!");
-    }
+	private void cadastrarPersonal() {
+	    System.out.print("Informe o nome do Personal: ");
+	    sc.nextLine();
+	    String nome = sc.nextLine();
 
-    private void cadastrarPlano() {
-        System.err.println("TBD");
-    }
+	    String cpf;
+	    boolean cpfValido;
 
-    private void cadastrarAluno() {
-        System.out.print("Informe o nome do Aluno: ");
-        sc.nextLine();
-        String nome = sc.nextLine();
+	    do {
+	        System.out.print("Informe o CPF (Apenas Números): ");
+	        cpf = sc.nextLine();
+	        cpf = cpf.replaceAll("[^0-9]", "");
+	        cpfValido = validarCPF(cpf);
 
-        String cpf;
-        boolean cpfValido;
-        LocalDate dataMatricula = LocalDate.now();
+	        if (!cpfValido) {
+	            System.out.println("Por favor, informe um CPF válido.");
+	        }
+	    } while (!cpfValido);
 
-        do {
-            System.out.print("Informe o CPF (Apenas Números): ");
-            cpf = sc.nextLine();
-            cpf = cpf.replaceAll("[^0-9]", "");
-            cpfValido = validarCPF(cpf);
+	    System.out.print("Informe a senha do Personal: ");
+	    String senha = sc.nextLine();
+	    System.out.print("Informe a especialidade do Personal: ");
+	    String espec = sc.nextLine();
+	    
+	    String cref;
+	    boolean crefValido;
+	    
+	    do {
+	        System.out.print("Informe a CREF do Personal (6 dígitos + G ou P + UF): ");
+	        cref = sc.nextLine();
+	        crefValido = cref.matches("\\d{6}[GP][A-Z]{2}");
+	        
+	        if (!crefValido) {
+	            System.out.println("Por favor, informe um CREF válido no formato: 6 dígitos + G ou P + UF");
+	        }
+	    } while (!crefValido);
+	    
+	    pessoas.add(new Personal(nome, cpf, senha, espec, cref));
+	    System.out.println("Personal " + nome + " cadastrado(a) com sucesso!");
+	}
 
-            if (!cpfValido) {
-                System.out.println("Por favor, informe um CPF válido.");
-            }
-        } while (!cpfValido);
-        
-        System.out.print("Informe a senha do Aluno: ");
-        String senha = sc.nextLine();
-        
-        Personal personalEncontrado = null;
+	private void cadastrarPlano() {
+		System.err.println("TBD");
+	}
 
-        do {
-            System.out.print("Informe o nome do Personal desejado: ");
-            String nomePersonal = sc.nextLine();
+	private void cadastrarAluno() {
+		System.out.print("Informe o nome do Aluno: ");
+		sc.nextLine();
+		String nome = sc.nextLine();
 
-            for (Pessoa p : pessoas) {
-                if (p instanceof Personal && p.getNome().equalsIgnoreCase(nomePersonal)) {
-                    personalEncontrado = (Personal) p;
-                    break;
-                }
-            }
+		String cpf;
+		boolean cpfValido;
+		LocalDate dataMatricula = LocalDate.now();
 
-            if (personalEncontrado == null) {
-                System.out.println("Personal não encontrado. Tente novamente.");
-            }
-        } while (personalEncontrado == null);
-        pessoas.add(new Aluno(nome, cpf, senha, dataMatricula, Plano.ANUAL_TOTAL, personalEncontrado));
-        System.out.println("Aluno " + nome + " cadastrado(a) com sucesso!");
-    }
+		do {
+			System.out.print("Informe o CPF (Apenas Números): ");
+			cpf = sc.nextLine();
+			cpf = cpf.replaceAll("[^0-9]", "");
+			cpfValido = validarCPF(cpf);
 
-    private void emitirRelatorio() {
-        System.err.println("TBD");
-    }
+			if (!cpfValido) {
+				System.out.println("Por favor, informe um CPF válido.");
+			}
+		} while (!cpfValido);
 
-    private static boolean validarCPF(String cpf) {
-        if (cpf.length() != 11) {
-            System.out.println("CPF inválido! Um CPF deve conter 11 dígitos.");
-            return false;
-        }
+		System.out.print("Informe a senha do Aluno: ");
+		String senha = sc.nextLine();
 
-        for (Pessoa pessoa : pessoas) {
-            if (pessoa.getCpf().equals(cpf)) {
-                System.out.println("CPF já cadastrado no sistema!");
-                return false;
-            }
-        }
-        return true;
-    }
+		List<Personal> personaisDisponiveis = new ArrayList<>();
+		System.out.println("\nPersonais disponíveis:");
+		System.out.println("0. Nenhum personal");
+
+		int index = 1;
+		for (Pessoa p : pessoas) {
+			if (p instanceof Personal) {
+				Personal personal = (Personal) p;
+				personaisDisponiveis.add(personal);
+				System.out.println(
+						index + ". " + personal.getNome() + " - Especialidade: " + personal.getEspecialidade());
+				index++;
+			}
+		}
+
+		Personal personalEscolhido = null;
+		int escolha;
+
+		do {
+			System.out.print("\nEscolha um personal pelo número (0 para nenhum): ");
+			escolha = sc.nextInt();
+			sc.nextLine();
+
+			if (escolha == 0) {
+
+				break;
+			} else if (escolha > 0 && escolha <= personaisDisponiveis.size()) {
+				personalEscolhido = personaisDisponiveis.get(escolha - 1);
+				break;
+			} else {
+				System.out.println("Opção inválida. Tente novamente.");
+			}
+		} while (true);
+
+		pessoas.add(new Aluno(nome, cpf, senha, dataMatricula, Plano.ANUAL_TOTAL, personalEscolhido));
+
+		if (personalEscolhido != null) {
+			System.out.println(
+					"Aluno " + nome + " cadastrado(a) com sucesso com o personal " + personalEscolhido.getNome() + "!");
+		} else {
+			System.out.println("Aluno " + nome + " cadastrado(a) com sucesso sem personal!");
+		}
+	}
+
+	private void emitirRelatorio() {
+		System.err.println("TBD");
+	}
+
+	private static boolean validarCPF(String cpf) {
+		if (cpf.length() != 11) {
+			System.out.println("CPF inválido! Um CPF deve conter 11 dígitos.");
+			return false;
+		}
+
+		for (Pessoa pessoa : pessoas) {
+			if (pessoa.getCpf().equals(cpf)) {
+				System.out.println("CPF já cadastrado no sistema!");
+				return false;
+			}
+		}
+		return true;
+	}
 }
